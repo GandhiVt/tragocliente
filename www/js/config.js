a061cd2fe0d348f87670e2b26788037ae0c95d09
GandhiVt <gandhi.vera@gmail.com>
2017-12-30 20:19:44 -0500

    primera
    primera

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+var krms_config ={			
+	'ApiUrl':"http://www.mascotitas.ga/mobileapp/api",		
+	'DialogDefaultTitle':"Pide tu trago",	
+	'pushNotificationSenderid':"",	
+	'facebookAppId':"",
+	'APIHasKey':"59bf731167e298f2c0eeafa2254f1020"
+};
+
