c350a16c20ec47d83b81eeee407765c224d42761
GandhiVt <gandhi.vera@gmail.com>
2018-01-17 23:09:53 -0500

    arreglos

--- www/js/config.js
+++ www/js/config.js
@@ -2,7 +2,7 @@ var krms_config ={
 	'ApiUrl':"https://mibar24.com/mobileapp/api",		
 	'DialogDefaultTitle':"Pide tu trago",	
 	'pushNotificationSenderid':"",	
-	'facebookAppId':"",
-	'APIHasKey':"2feff931f33e53b800bea391f13be11b"
+	'facebookAppId':"208153039733325",
+	'APIHasKey':"f0e835c1ae957f1eeb3f85937b19f248"
 };
 
