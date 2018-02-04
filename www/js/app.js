1ed9a08f27eb1c0432c6899efa6c17a31b33b94d
GandhiVt <gandhi.vera@gmail.com>
2018-02-04 14:24:16 -0500

    das

--- www/js/app.js
+++ www/js/app.js
@@ -39,9 +39,11 @@ var timer = null;
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {    
-	    					
+	   					
 	navigator.splashscreen.hide();
-	
+	initAd();
+	showBannerFunc();
+	showInterstitialFunc();
 	if(!isDebug()){
  	   getLanguageSettings();
 	}				
@@ -9111,4 +9113,58 @@ function iOSeleven()
 		}
 	}
 	return false;
+}
+
+//initialize the goodies 
+function initAd(){
+        if ( window.plugins && window.plugins.AdMob ) {
+            var ad_units = {
+                ios : {
+                    banner: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx',		//PUT ADMOB ADCODE HERE 
+                    interstitial: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx'	//PUT ADMOB ADCODE HERE 
+                },
+                android : {
+                    banner: 'ca-app-pub-3332475105709099/9388906068',		//PUT ADMOB ADCODE HERE 
+                    interstitial: 'ca-app-pub-3332475105709099/9330740906'	//PUT ADMOB ADCODE HERE 
+                }
+            };
+            var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
+ 
+            window.plugins.AdMob.setOptions( {
+                publisherId: admobid.banner,
+                interstitialAdId: admobid.interstitial,
+                adSize: window.plugins.AdMob.AD_SIZE.SMART_BANNER,	//use SMART_BANNER, BANNER, LARGE_BANNER, IAB_MRECT, IAB_BANNER, IAB_LEADERBOARD 
+                bannerAtTop: false, // set to true, to put banner at top 
+                overlap: true, // banner will overlap webview 
+                offsetTopBar: false, // set to true to avoid ios7 status bar overlap 
+                isTesting: false, // receiving test ad 
+                autoShow: true // auto show interstitial ad when loaded 
+            });
+ 
+            registerAdEvents();
+        } else {
+            //alert( 'admob plugin not ready' ); 
+        }
+}
+//functions to allow you to know when ads are shown, etc. 
+function registerAdEvents() {
+        document.addEventListener('onReceiveAd', function(){});
+        document.addEventListener('onFailedToReceiveAd', function(data){});
+        document.addEventListener('onPresentAd', function(){});
+        document.addEventListener('onDismissAd', function(){ });
+        document.addEventListener('onLeaveToAd', function(){ });
+        document.addEventListener('onReceiveInterstitialAd', function(){ });
+        document.addEventListener('onPresentInterstitialAd', function(){ });
+        document.addEventListener('onDismissInterstitialAd', function(){ });
+    }
+
+
+    //display the banner 
+function showBannerFunc(){
+    window.plugins.AdMob.createBannerView();
+}
+//display the interstitial 
+function showInterstitialFunc(){
+    window.plugins.AdMob.createInterstitialView();	//get the interstitials ready to be shown and show when it's loaded. 
+    window.plugins.AdMob.requestInterstitialAd();
 }
